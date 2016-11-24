373621d5fd1fd7a44762d7383ca2e649b0b5d8b5
Ita Lifshitz <italif@gmail.com>
2016-11-24 14:36:58 +0200

    initial commit

--- demo.m
+++ demo.m
@@ -1,8 +1,9 @@
 function demo()
 % Human Pose Estimation demo function
 
-MATCAFFE = '/usr/wisdom/caffe/matlab'; %replace with your caffe matlab folder
+MATCAFFE = '/home/ita/code/caffe4/matlab'; %replace with your caffe matlab folder
 TRW_S = '/home/ita/code/TRW_S-v1.3/'; %replace with your TRW-S folder
+CAFFE_MODEL = 'HPE-WIS.caffemodel';
 
 if (~exist(MATCAFFE,'dir'))
     error('please install caffe: https://github.com/BVLC/caffe');
@@ -14,6 +15,10 @@ if (~exist(TRW_S,'dir'))
     error('please install TRW-S: https://github.com/markeroon/matlab-computer-vision-routines/blob/master/third_party/imrender/vgg/vgg_trw_bp.m');
 else
     addpath(genpath(TRW_S));
+end
+
+if (~exist(fullfile('net',CAFFE_MODEL),'file'))
+    error('please copy into folder "net" model file: http://www.wisdom.weizmann.ac.il/~vision/deep-voting-hpe/HPE-WIS.caffemodel');
 end
 
 imageDir = '.';
